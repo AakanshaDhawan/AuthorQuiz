b5024b8e01c8cfc062794a7b5c24d338f978d4c8
aakansha <aakansha@mobiux.in>
2020-04-14 20:08:56 +0530

    new changes

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -3,9 +3,61 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css'
 
-function AuthorQuiz() {
+function Hero(){
   return (
-    <div> Author Quiz
+    <div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1> AuthorQuiz</h1>
+      <p>Select the book written by the author shown</p>
+    </div>
+    </div>
+  );
+}
+
+function Book({title}){
+  return(
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+function Turn({author,books}) {
+  return(<div className="row turn" style={{backgroundColor: "white"}}>
+    <div className="col-4 offset-1">
+      <img src={author.imageUrl} className="authorImage" alt="Author" />
+    </div>
+    <div className="col-5 offset-1">
+      {books.map((title) => 
+        <Book title= {title} key={title}/>
+        )}
+    </div>
+
+  </div>)
+}
+
+function Continue() {
+  return(<div/>)
+}
+
+function Footer() {
+  return( 
+  <div id="footer" className="row">
+  <div className="col-12">
+    <p className="text-muted credit">
+      All images are from <a href="www.google.com">Google </a>
+    </p>
+  </div>
+  </div>)
+}
+
+function AuthorQuiz({turnData}) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} />
+      <Continue />
+      <Footer />
     </div>
   );
 }
